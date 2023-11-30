eb2235acd43135869a6bf2a105b7772d0bf5c129
klau55 <kipatok@bk.ru>
2023-11-30 16:27:53 +0200

    rdy 4 eval

--- srcs/ft_printf.h
+++ srcs/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nkarpilo <nkarpilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:08:40 by nkarpilo          #+#    #+#             */
-/*   Updated: 2023/11/29 17:32:22 by nkarpilo         ###   ########.fr       */
+/*   Updated: 2023/11/30 16:20:44 by nkarpilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include "libft/libft.h"
 
 int		ft_printf(const char *format, ...);
 int		ft_processor(va_list args, const char *format);
@@ -27,5 +28,6 @@ int		ft_print_str(char *s, int total);
 int		ft_print_hex(unsigned long n, const char format, int total);
 int		ft_print_uint(unsigned int n, int total);
 int		ft_print_ptr(void *ptr, int total);
+int		ft_preprocessor(const char *format);
 
 #endif
